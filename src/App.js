58acbe68b80e171eda18fb00310a90bfda9eec80
Iswar <rick731202@gmail.com>
2021-11-01 12:09:12 +0530

    another initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
+import Home from "./pages/Home";
+import AboutUs from "./pages/AboutUs";
+import ContactUs from "./pages/ContactUs";
+import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
 
-function App() {
+function App()
+{
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <NavBar />
+        {/* Padding form the navbar */}
+        <div style={{height: "17vh",}}></div>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about-us" component={AboutUs} />
+          <Route path="/contact-us" component={ContactUs} />
+        </Switch>
+        <Footer />
+      </Router>
+    </>
   );
 }
 

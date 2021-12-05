6cbd2a47b44cea35e68c08588d6623d37372f4e2
Iswar <rick731202@gmail.com>
2021-12-05 20:55:35 +0530

    pushed the footer to the bottom using min-height

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -3,7 +3,7 @@ import React from 'react'
 function AboutUs()
 {
     return (
-        <div>
+        <div className="main-content-wrapper">
             <h2>This is the about us page</h2>
         </div>
     )
